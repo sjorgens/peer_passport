fe64624ed36ad53081401af65d3393893033f6c7
Robby Newman <robbynewman@Robbys-MacBook-Pro.local>
2016-01-26 16:51:36 -0600

    fixed stuff - typos and whatnot

--- server/public/assets/scripts/client.js
+++ server/public/assets/scripts/client.js
@@ -2,12 +2,12 @@
  * Created by robbynewman on 1/26/16.
  */
 
-var app = angular.module('getApp', []);
+var app = angular.module('myApp', []);
 
 
 app.controller('MainController', function($scope, $http){
     $http.get('getUser').then(function(response){
-        console.log(response);
+        console.log('response', response);
         $scope.user = response
 
     });
